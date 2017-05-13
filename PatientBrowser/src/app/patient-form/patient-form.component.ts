7dcf7c14f800642c77803cbc0021a3d9b4a847d0
Kamil Zielinski <K.Zielinski@kainos.com>
2017-05-13 22:22:05 +0200

    added generic patient form component

---
+++ PatientBrowser/src/app/patient-form/patient-form.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, AbstractControl, Validators } from '@angular/forms';
+
+import { Patient } from '../../models/patient';
+
+@Component({
+  selector: 'app-patient-form',
+  templateUrl: './patient-form.component.html',
+  styleUrls: ['./patient-form.component.css']
+})
+export class PatientFormComponent implements OnInit {
+
+  @Input() patient: Patient;
+  @Input() header: string;
+  @Output() onSubmitted: EventEmitter<Patient> = new EventEmitter();
+
+  form: FormGroup;
+  nameControl: AbstractControl;
+  surnameControl: AbstractControl;
+
+  constructor(formBuilder: FormBuilder) {
+    if (this.patient == null) {
+      this.patient = new Patient('', '');
+    }
+
+    this.form = formBuilder.group({
+      'name': ['', Validators.required],
+      'surname': ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5)])
+      ]
+    });
+
+    this.nameControl = this.form.controls['name'];
+    this.surnameControl = this.form.controls['surname'];
+  }
+
+  ngOnInit() { }
+
+  onSubmit(values: any) {
+    console.log(`Hello from onSubmit ${values.name} ${values.surname} `);
+    this.patient.name = values.name;
+    this.patient.surname = values.surname;
+    this.onSubmitted.emit(this.patient);
+  }
+}

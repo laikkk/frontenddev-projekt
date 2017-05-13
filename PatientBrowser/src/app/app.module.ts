f54244174fc7e8f83e9dd2633a38d6b770ee7f6d
Kamil Zielinski <K.Zielinski@kainos.com>
2017-05-13 22:26:05 +0200

    remove unneeded patient-edit component (using patient-form)

--- PatientBrowser/src/app/app.module.ts
+++ PatientBrowser/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PatientListComponent } from './patient-list/patient-list.component';
-import { PatientEditComponent } from './patient-edit/patient-edit.component';
 import { PatientFormComponent } from './patient-form/patient-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PatientListComponent,
-    PatientEditComponent,
     PatientFormComponent
   ],
   imports: [

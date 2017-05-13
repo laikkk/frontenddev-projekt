12ae011e5c562b61487e12c0a27ddaeea03e4962
Kamil Zielinski <K.Zielinski@kainos.com>
2017-05-13 22:23:40 +0200

    added patient form component to app module with reactive forms

--- PatientBrowser/src/app/app.module.ts
+++ PatientBrowser/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PatientListComponent } from './patient-list/patient-list.component';
 import { PatientEditComponent } from './patient-edit/patient-edit.component';
+import { PatientFormComponent } from './patient-form/patient-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PatientListComponent,
-    PatientEditComponent
+    PatientEditComponent,
+    PatientFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
